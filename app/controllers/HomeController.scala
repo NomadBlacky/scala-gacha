8baf0f5f048287c79e26223c08f2636e53df7b49
NomadBlacky <hourou.hamurinzu@gmail.com>
2018-01-06 17:41:06 +0900

    Add a sample of Json response

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -1,7 +1,8 @@
 package controllers
 
 import javax.inject._
-import play.api._
+
+import play.api.libs.json.Json
 import play.api.mvc._
 
 /**
@@ -24,5 +25,14 @@ class HomeController @Inject()(cc: ControllerComponents) extends AbstractControl
 
   def hello(name: String) = Action {
     Ok(views.html.hello(name))
+  }
+
+  def json = Action {
+    Ok(Json.toJson(
+      Map(
+        "a" -> List(10),
+        "b" -> List(2, 3, 4)
+      )
+    ))
   }
 }
